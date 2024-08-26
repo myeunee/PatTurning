a6e85c78f910355a9b1903d740e56339d60d4610
SangYeong Park <tkddud386@gmail.com>
2024-08-26 17:03:03 +0900

    fix: respond prices

--- server/src/main/java/com/swdc/server/domain/mongoDB/Price.java
+++ server/src/main/java/com/swdc/server/domain/mongoDB/Price.java
@@ -3,10 +3,14 @@ package com.swdc.server.domain.mongoDB;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class Price {
+    @Id
+    private Integer id;
+
 
 }
