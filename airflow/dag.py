a211c7f1d9d91525821af1dbd5009523c190a406
hyejiyu <dbgpwl34@gmail.com>
2024-09-18 23:27:04 +0900

    CI/CD test02

--- airflow/dag.py
+++ airflow/dag.py
@@ -53,5 +53,3 @@ with DAG(
    
     category_ids = list(range(100001, 100078))
     send_post_request_HOMEPLUS_task.expand(category_id=category_ids)
-
-##############################################test###########################################
