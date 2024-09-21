797069d5dc3dcd720c7a686f47a3a38993dd28f6
hyejiyu <dbgpwl34@gmail.com>
2024-09-21 20:37:46 +0900

    [In Progress] add Sending Email Configs...

--- airflow/dag.py
+++ airflow/dag.py
@@ -30,11 +30,13 @@ def send_post_request(categoryId):
 
 # DAG의 기본 설정
 default_args = {
-    'owner': 'khuda',                  # DAG 소유자
-    'depends_on_past': False,          # 이전 DAG 실패 여부에 의존하지 않음
-    'email_on_failure': False,         # 실패 시 이메일 보내기 여부
-    'email_on_retry': False,           # 재시도 시 이메일 보내기 여부
-    'retries': 1,                      # 실패 시 재시도 횟수
+    'owner': 'khuda',                   # DAG 소유자
+    'depends_on_past': False,           # 이전 DAG 실패 여부에 의존하지 않음
+    'email': ['dbgpwl34@gmail.com'],    # 수신자 이메일
+    "email_on_success": True,           # 성공 시 이메일 전송
+    'email_on_failure': True,           # 실패 시 이메일 전송
+    'email_on_retry': True,             # 재시도 시 이메일 전송
+    'retries': 1,                       # 실패 시 재시도 횟수
     'retry_delay': timedelta(minutes=5) # 재시도 간격
 }
 
@@ -44,7 +46,7 @@ with DAG(
     default_args=default_args,           # 기본 인자 설정
     description='HomePlus Crawling',     # DAG 설명
     schedule_interval=timedelta(days=1), # 실행 주기 (매일 1회)
-    start_date=datetime(2024, 9, 18),    # 시작 날짜
+    start_date=datetime(2024, 9, 20),    # 시작 날짜
     catchup=False                        # 시작 날짜부터 현재까지의 미실행 작업 실행 여부
 ) as dag:
     
@@ -69,7 +71,7 @@ with DAG(
     @task
     def send_post_request_HOMEPLUS_task(category_id):
         return send_post_request(category_id)    
-   
+
     category_ids = list(range(100001, 100078))
 
     [run_consumer_task1, run_consumer_task2, run_consumer_task3, send_post_request_HOMEPLUS_task.expand(category_id=category_ids)]
