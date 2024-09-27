c1486a33b1c69551d733ef40e064bd80ad5b0e40
SangYeong Park <107484383+Imsyp@users.noreply.github.com>
2024-09-27 21:43:56 +0900

    Update PriceService.java

--- server/src/main/java/com/swdc/server/service/PriceService.java
+++ server/src/main/java/com/swdc/server/service/PriceService.java
@@ -34,7 +34,7 @@ import java.util.stream.Collectors;
 public class PriceService {
 
     private static final Logger logger = LoggerFactory.getLogger(PriceService.class);
-    private static final String BASE_PATH = "/mnt/filestore";
+    private static final String BASE_PATH = "/mnt/patturning";
 
     @Autowired
     private MongoTemplate mongoTemplate;
