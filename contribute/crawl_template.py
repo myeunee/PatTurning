6d080afbdc7551d06dff4d0f9d4c06a0fcbee852
hyejiyu <dbgpwl34@gmail.com>
2024-10-11 00:43:58 +0900

    added crawl_template guides

---
+++ contribute/crawl_template.py
@@ -0,0 +1,12 @@
+''' 
+mq_producer_template.py 파일에 RabbitMQProducer 클래스를 참고하시어 
+크롤링 코드를 작성하시어 주십시오.
+
+환경변수는 다음과 같이 정의되어 있습니다.
+username = os.getenv('RABBITMQ_USERNAME')
+password = os.getenv('RABBITMQ_PASSWORD')
+host = os.getenv('RABBITMQ_HOSTNAME')
+port = os.getenv('RABBITMQ_PORT')
+
+사용자 지정 큐는 {추후 링크 추가} 링크를 참고하시어 Issues로 요청하시어 주십시오.
+'''
